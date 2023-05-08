d23567118e1de35ce1de659fe68ad644388c0862
headironc <i@liangzhicheng.cc>
2023-05-08 15:17:51 +0800

    refactor: remove antd and styled-components

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,6 @@ import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "react-router-dom";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { App, ConfigProvider } from "antd";
-import zhCN from "antd/lib/locale/zh_CN";
 
 import "antd/dist/reset.css";
 
@@ -17,11 +15,7 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById("root") as HTMLElement).render(
     <StrictMode>
         <QueryClientProvider client={queryClient}>
-            <ConfigProvider locale={zhCN}>
-                <App>
-                    <RouterProvider router={router} />
-                </App>
-            </ConfigProvider>
+            <RouterProvider router={router} />
             <ReactQueryDevtools initialIsOpen={false} />
         </QueryClientProvider>
     </StrictMode>
